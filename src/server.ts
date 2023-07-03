6e91a586351d99e53d02043fdfbb1f2e9d3f7ebe
khawaja-ammar <mammarkh@gmail.com>
2023-07-03 15:42:10 +0500

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
-require('dotenv').config();
-const PORT = process.env.PORT || 3500;
+import * as dotenv from 'dotenv';
+import express from 'express';
+import https from 'https';
+import cors from 'cors';
+import fs from 'fs';
 
-const express = require('express');
+dotenv.config();
+const PORT = +process.env.PORT || 3500;
 
-const app = express();
-const http = require('http').Server(app);
-const cors = require('cors');
-
+// SSL/TLS config
+const httpsOptions = {
+  //   key: fs.readFileSync(''),
+  //   cert: fs.readFileSync(''),
+  //   // ca: ""
+};
 const corsOptions = require('./config/corsOptions');
 
-http.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+const app = express();
+// Middleware
+app.use(express.json());
+// app.use(cors(), corsOptions);
+
+// API
+// TODO: Api routes and controllers
+
+// Start after after DB connection
+const server = https.createServer(httpsOptions, app);
+server.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
